46bac8367ac2c9975c9eda0d34851bf691ff6949
DANCAN-OPENJI-DEVELOPS <114863301+DANCAN-OPENJI-DEVELOPS@users.noreply.github.com>
2022-10-29 13:39:12 +0300

    Create tests.js

---
+++ tests.js
@@ -0,0 +1,13 @@
+palindrome("eye") should return a boolean.
+Passed:palindrome("eye") should return true.
+Passed:palindrome("_eye") should return true.
+Passed:palindrome("race car") should return true.
+Passed:palindrome("not a palindrome") should return false.
+Passed:palindrome("A man, a plan, a canal. Panama") should return true.
+Passed:palindrome("never odd or even") should return true.
+Passed:palindrome("nope") should return false.
+Passed:palindrome("almostomla") should return false.
+Passed:palindrome("My age is 0, 0 si ega ym.") should return true.
+Passed:palindrome("1 eye for of 1 eye.") should return false.
+Passed:palindrome("0_0 (: /-\ :) 0-0") should return true.
+Passed:palindrome("five|\_/|fou
